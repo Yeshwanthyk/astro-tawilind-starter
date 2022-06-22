b0c0915bf3a9f00cd65377089b10500bf469b770
Yesh  (Keerthi Yendamuri) <yeshwanth.yk@gmail.com>
2022-06-23 01:06:56 +0530

    Fix pagination

--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -2,15 +2,15 @@ import React from "react";
 
 const BlogPost = ({ title, description, slug }) => {
   return (
-    <div class="w-full mb-8">
-      <div class="flex flex-col justify-between md:flex-row">
-        <a class="decoration-sky-700" href={`/blog/${slug}`}>
-          <h4 class="w-full mb-2 text-lg font-medium text-gray-900 md:text-xl ">
+    <div className="w-full mb-8">
+      <div className="flex flex-col justify-between md:flex-row">
+        <a className="decoration-sky-700" href={`/blog/${slug}`}>
+          <h4 className="w-full mb-2 text-lg font-medium text-gray-900 md:text-xl ">
             {title}
           </h4>
         </a>
       </div>
-      <p class="text-gray-600 dark:text-gray-400">{description}</p>
+      <p className="text-gray-600 dark:text-gray-400">{description}</p>
     </div>
   );
 };
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,9 @@ module.exports = {
   mode: "jit",
   content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
   theme: {
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [require("@tailwindcss/typography")],
